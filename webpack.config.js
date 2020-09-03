5103c721b515710f0dfec45614dc8f19f3c86f0c
Rui Valim <r.valim.junior@gmail.com>
2020-09-02 23:52:57 -0300

    Project Init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+	devServer: {
+		hot: true,
+		historyApiFallback: true,
+		port: 3300
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: "ts-loader"
+			}
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		publicPath: '/'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+			filename: "./index.html"
+		}),
+		new CompressionPlugin()
+	]
+}
